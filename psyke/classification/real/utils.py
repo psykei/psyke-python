abf855c4f379ec50554d33e3781f9cf5a4b17481
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2021-12-29 23:50:10 +0100

    fixed optimisation and updated tests

--- psyke/classification/real/utils.py
+++ psyke/classification/real/utils.py
@@ -15,9 +15,9 @@ class Rule:
                all([predicate in self.false_predicates for predicate in rule.false_predicates])
 
     def reduce(self, features: Iterable[DiscreteFeature]) -> Rule:
-        to_be_removed = [feature.admissible_values.keys() for tp in self.true_predicates
-                         for feature in features if tp in feature.admissible_values]
-        to_be_removed = [item for sublist in to_be_removed for item in sublist]
+        to_be_removed = [item for tp in self.true_predicates
+                         for feature in features if tp in feature.admissible_values
+                         for item in feature.admissible_values.keys()]
         return Rule(self.true_predicates, [fp for fp in self.false_predicates if fp not in to_be_removed])
 
     def to_lists(self) -> list[list[str]]:
@@ -30,26 +30,17 @@ class IndexedRuleSet(dict[int, list[Rule]]):
         return [(key, value) for key, values in self.items() for value in values]
 
     def optimize(self) -> IndexedRuleSet:
-        useless_rules = [IndexedRuleSet._useless_rules(key, entry) for key, entry in self.items()]
-        useless_rules = [] if len(useless_rules) == 0 else [item for sublist in useless_rules for item in sublist]
+        useless_rules = [item for key, entry in self.items() for item in IndexedRuleSet._useless_rules(key, entry)]
         for rule in useless_rules:
             self[rule[0]].remove(rule[1])
         return self
 
     @staticmethod
     def _useless_rules(key, rules: list[Rule]) -> list[(int, Rule)]:
-        result = []
-        if len(rules) > 1:
-            for rule in rules:
-                append = True
-                for other_rule in rules:
-                    if (rule != other_rule) and not rule.is_sub_rule_of(other_rule):
-                        append = False
-                        break
-                if append:
-                    result.append((key, rule))
-        return result
-        # return [(key, rule) for rule in rules if not any(rule.is_sub_rule_of(aux) for aux in rules if aux != rule)]
+        return [
+            (key, rule) for rule in rules
+            if any(rule.is_sub_rule_of(other_rule) for other_rule in rules if other_rule != rule)
+        ]
 
     @staticmethod
     def create_indexed_ruleset(dataset: pd.DataFrame) -> IndexedRuleSet:
