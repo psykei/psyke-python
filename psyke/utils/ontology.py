54c0949e0f4f7bc4c9cce941befbd6986ff94db9
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2022-01-23 04:24:00 +0100

    fixed REAL

--- psyke/utils/ontology.py
+++ psyke/utils/ontology.py
@@ -2,6 +2,7 @@ import numbers
 from owlready2 import *
 import pandas as pd
 from pandas.core.dtypes.common import is_numeric_dtype, is_string_dtype
+from tuprolog.core import Struct
 from tuprolog.theory import Theory
 
 
@@ -9,26 +10,21 @@ def dataframe_to_ontology(dataframe: pd.DataFrame) -> owlready2.Ontology:
     name = dataframe.columns[-1]
     ontology = get_ontology(f"{name[0].lower() + name[1:]}.rdf")
     with ontology:
-        Main = type(name.capitalize(), (Thing,), {})
         for column in dataframe.columns:
             if is_numeric_dtype(dataframe[column]):
-                type(column, (Main >> float, FunctionalProperty), {})
+                t = float
             elif is_string_dtype(dataframe[column]):
-                type(column, (Main >> str, FunctionalProperty), {})
+                t = str
             else:
                 raise TypeError
+            type(column, (type(name.capitalize(), (Thing,), {}) >> t, FunctionalProperty), {})
     return ontology
 
 
 def individuals_from_dataframe(ontology: owlready2.Ontology, dataframe: pd.DataFrame) -> list:
-    # owlready2.Ontology:
-    # individuals = get_ontology(f"{ontology.name}_individuals.rdf")
-    # individuals.imported_ontologies.append(ontology)
-    Main = type(ontology.name.capitalize(), (Thing,), {})
-    # with ontology:
     ret = []
     for _, row in dataframe.iterrows():
-        string_row = "ontology.Iris("
+        string_row = "list(ontology.classes())[0]("
         for name, value in zip(row.index, row):
             if isinstance(value, numbers.Number):
                 string_row += f"{name}={value},"
@@ -36,35 +32,46 @@ def individuals_from_dataframe(ontology: owlready2.Ontology, dataframe: pd.DataF
                 string_row += f"{name}='{value}',"
             else:
                 raise TypeError
-        individual = eval(string_row[:-1] + ")")
-        ret.append(individual)
-        # destroy_entity(individual)
-    # ret = list(individuals.individuals())
-    # individuals.destroy()
+        evaluated_row = eval(string_row[:-1] + ")")
+        ret.append(evaluated_row)
     return ret
+
+
+def rules_from_struct(struct: Struct, rules: list[str] = None) -> list[str]:
+    if rules is None:
+        rules = [""]
+    if struct.is_recursive:
+        return rules_from_struct(struct.args[0], rules_from_struct(struct.args[1], rules))
+    if struct.functor == "=<":
+        rules = [r + f"lessThanOrEqual(?{struct.args[0].name},{struct.args[1].value})," for r in rules]
+    elif struct.functor == '>':
+        rules = [r + f"greaterThan(?{struct.args[0].name},{struct.args[1].value})," for r in rules]
+    elif struct.functor == 'in':
+        rules = [r +
+                 f"greaterThan(?{struct.args[0].name},{struct.args[1][0].value})," +
+                 f"lessThanOrEqual(?{struct.args[0].name},{struct.args[1][1][0].value})," for r in rules]
+    elif struct.functor == 'not_in':
+        rules = [r + f"lessThanOrEqual(?{struct.args[0].name},{struct.args[1][0].value})," for r in rules] +\
+                [r + f"greaterThan(?{struct.args[0].name},{struct.args[1][1][0].value})," for r in rules]
+    else:
+        raise TypeError
+    return rules
 
 
 def rules_from_theory(ontology: owlready2.Ontology, theory: Theory, dataframe: pd.DataFrame) -> owlready2.Ontology:
     name = ontology.name
-    # name = dataframe.columns[-1]
     lower_name = name[0].lower() + name[1:]
-    Main = type(name.capitalize(), (Thing,), {})
     with ontology:
-        Imp().set_as_rule(eval("Main").name +
-                          f"""(?{lower_name}), PetalLength(?{lower_name}, ?pl), SepalLength(?{lower_name}, ?sl), 
-            PetalWidth(?{lower_name}, ?pw), SepalWidth(?{lower_name}, ?sw), greaterThanOrEqual(?pl, 5) -> 
-            {lower_name}(?{lower_name}, 'versicolor')""")
-        Imp().set_as_rule(eval("Main").name +
-                          f"""(?{lower_name}), PetalLength(?{lower_name}, ?pl), SepalLength(?{lower_name}, ?sl), 
-                    PetalWidth(?{lower_name}, ?pw), SepalWidth(?{lower_name}, ?sw), lessThan(?pl, 5) -> 
-                    {lower_name}(?{lower_name}, 'virginica')""")
-        # rule2.set_as_rule(
-        #     """Iris(?i), PetalLength(?i, ?pl), SepalLength(?i, ?sl), PetalWidth(?i, ?pw), SepalWidth(?i, ?sw),
-        #     lessThan(?pl, 5) -> iris(?i, 'versicolor')""")
-    # for c in theory:
-    #    print(c.head)
-    #    for h in c.head:
-    #        print(h.name)
-    #    print(c.body)
-    #    break
+        for clause in theory:
+            string_rule = list(ontology.classes())[0].name + f"(?{lower_name}),"
+            for arg in clause.head.args[:-1]:
+                string_rule += f"{arg.name}(?{lower_name},?{arg.name}),"
+            if clause.body.arity > 0:
+                body = rules_from_struct(clause.body)
+                rules = [string_rule + b for b in body]
+            else:
+                rules = [string_rule]
+            for rule in rules:
+                string_rule = f"{rule[:-1]}->{lower_name}(?{lower_name},'{clause.head.args[-1]}')"
+                Imp().set_as_rule(string_rule)
     return ontology
