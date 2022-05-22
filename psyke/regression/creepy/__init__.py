3298ce6b63b8cc09ee46dc7342b9935813c21ae3
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2022-05-22 02:26:09 +0200

    added data sets

--- psyke/regression/creepy/__init__.py
+++ psyke/regression/creepy/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 from numpy import ndarray
 from sklearn.linear_model import LinearRegression
 from sklearn.mixture import GaussianMixture
@@ -9,9 +8,9 @@ from tuprolog.theory import Theory
 from psyke.regression import ClusterExtractor, Node, ClosedCube, HyperCubeExtractor
 
 
-class CREAM(ClusterExtractor):
+class CReEPy(ClusterExtractor):
     """
-    Explanator implementing CREAM algorithm.
+    Explanator implementing CReEPy algorithm.
     """
 
     def __init__(self, predictor, depth: int, dbscan_threshold: float, error_threshold: float, constant: bool = False):
@@ -29,17 +28,9 @@ class CREAM(ClusterExtractor):
             left, self._calculate_error(data.iloc[~indices, :-1], left)
 
     def __eligible_cubes(self, gauss_pred: ndarray, node: Node):
-        cubes = []
-        for inner_cube in [self._create_cube(node.dataframe.iloc[np.where(gauss_pred == i)]) for i in range(2)]:
-            indices = self.__indices(inner_cube, node.dataframe)
-            if indices is None:
-                continue
-            right, right_e, left, left_e = self.__split_with_errors(inner_cube, node.cube, node.dataframe, indices)
-            cubes.append((
-                ((right_e + left_e) / 2, right.volume(), left.volume()),
-                (right, indices, right_e), (left, ~indices, left_e)
-            ))
-        return cubes
+        cubes = [self._create_cube(node.dataframe.iloc[np.where(gauss_pred == i)]) for i in range(2)]
+        indices = [self.__indices(cube, node.dataframe) for cube in cubes]
+        return cubes, indices
 
     @staticmethod
     def __indices(cube: ClosedCube, data: pd.DataFrame) -> ndarray | None:
@@ -55,26 +46,30 @@ class CREAM(ClusterExtractor):
             (_, depth, node) = to_split.pop()
             gauss_pred = GaussianMixture(n_components=2).fit_predict(node.dataframe)
 
-            cubes = self.__eligible_cubes(gauss_pred, node)
+            cubes, indices = self.__eligible_cubes(gauss_pred, node)
+            cubes = [(c.volume(), len(idx), idx, c) for c, idx in zip(cubes, indices) if idx is not None]
+
             if len(cubes) < 1:
                 continue
-            _, right, left = min(cubes)
-            node.right = Node(node.dataframe[right[1]], right[0])
-            node.cube.update(node.dataframe[left[1]], self.predictor)
-            node.left = Node(node.dataframe[left[1]], left[0])
+            _, _, indices, cube = max(cubes)
+            if node.cube.equal(cube):
+                _, _, indices, cube = min(cubes)
 
-            if depth < self.depth:
-                to_split += [
-                    (error, depth + 1, n)
-                    for (n, error) in zip(node.children, [right[2], left[2]]) if error > self.error_threshold
-                ]
+            cube.update(node.dataframe[indices], self.predictor)
+            node.right = Node(node.dataframe[indices], cube)
+            node.cube.update(node.dataframe[~indices], self.predictor)
+            node.left = Node(node.dataframe[~indices], node.cube)
+            error = self._calculate_error(node.dataframe.iloc[indices, :-1], cube)
 
-            #plt.scatter(node.dataframe.X, node.dataframe.Y,
-            #            c=gauss_pred, s=.5)
-            #plt.gca().set_aspect('equal')
-            #plt.xlim((0, 1))
-            #plt.ylim((0, 1))
-            #plt.show()
+            if depth < self.depth and error > self.error_threshold:
+                to_split.append((error, depth + 1, node.right))
+
+        #    plt.scatter(node.dataframe.X, node.dataframe.Y,
+        #                c=gauss_pred, s=.5)
+        #    plt.gca().set_aspect('equal')
+        #    plt.xlim((0, 1))
+        #    plt.ylim((0, 1))
+        #    plt.show()
 
     def _calculate_error(self, dataframe: pd.DataFrame, cube: ClosedCube) -> float:
         output = cube.output
