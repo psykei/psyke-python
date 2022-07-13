3436ba28bdf61360845fe519cc27c1cd7e2eafe0
Matteo Magnini <matteo.magnini@studio.unibo.it>
2022-07-13 18:30:30 +0200

    feat(cart): refactoring of cart algorithm. This closes #2

--- test/psyke/cart/test_simplified_cart.py
+++ test/psyke/cart/test_simplified_cart.py
@@ -6,7 +6,9 @@ from test import get_dataset, get_model
 import unittest
 
 
-@parameterized_class([{"dataset": "iris", "predictor": "DTC"}, {"dataset": "house", "predictor": "DTR"}])
+# TODO: should be refactored using the a .csv file
+@parameterized_class([{"dataset": "iris", "predictor": "DTC", "task": "classification"},
+                      {"dataset": "house", "predictor": "DTR", "task": "regression"}])
 class TestSimplifiedCart(unittest.TestCase):
 
     def test_equality(self):
@@ -15,9 +17,9 @@ class TestSimplifiedCart(unittest.TestCase):
         train, test = train_test_split(dataset, test_size=0.5)
         tree = get_model(self.predictor, {})
         tree.fit(train.iloc[:, :-1], train.iloc[:, -1])
-        extractor = Extractor.cart(CartPredictor(tree), simplify=False)
+        extractor = Extractor.cart(CartPredictor(tree), simplify=False, task=self.task)
         _ = extractor.extract(train)
-        simplified_extractor = Extractor.cart(CartPredictor(tree))
+        simplified_extractor = Extractor.cart(CartPredictor(tree), task=self.task)
         _ = simplified_extractor.extract(train)
         self.assertEqual(
             list(extractor.predict(test.iloc[:, :-1])),
