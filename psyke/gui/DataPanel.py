e3984d71ff68a0981fd8878b8a66865716cd7955
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2022-08-12 20:25:36 +0200

    [wip]

--- psyke/gui/DataPanel.py
+++ psyke/gui/DataPanel.py
@@ -4,20 +4,18 @@ from kivy.uix.label import Label
 from kivy.uix.spinner import Spinner
 from kivy.uix.togglebutton import ToggleButton
 
-from psyke.gui import HorizontalBoxLayout, VerticalBoxLayout
-from psyke.gui.model import TASKS, DATASET_MESSAGE, DATASETS, INFO_DATASET_MESSAGE
+from psyke.gui.layout import PanelBoxLayout, SidebarBoxLayout, HorizontalBoxLayout
+from psyke.gui.model import TASKS, DATASET_MESSAGE, DATASETS, INFO_DATASET_MESSAGE, INFO_DATASET_PREFIX
 
 
-class DataPanel(HorizontalBoxLayout):
+class DataPanel(PanelBoxLayout):
 
     def __init__(self, controller, **kwargs):
-        super().__init__(size_hint_y=None, height=200, **kwargs)
-        self.controller = controller
+        super().__init__(controller, 'Load', INFO_DATASET_MESSAGE, **kwargs)
         self.controller.set_data_panel(self)
 
         self.task = TASKS[0]
         self.dataset = None
-        self.info_label = Label(text=INFO_DATASET_MESSAGE)
         self.preprocessing = {}
 
         task_panel = HorizontalBoxLayout()
@@ -26,68 +24,50 @@ class DataPanel(HorizontalBoxLayout):
             btn_task.bind(state=self.select_task)
             task_panel.add_widget(btn_task)
 
-        self.data_options = Spinner(
-            size_hint_x=None, width=130, pos_hint={'center_x': .5, 'center_y': .5}
-        )
-        self.data_options.bind(text=self.select_dataset)
-        self.load_button = Button(text='Load', disabled=True, on_press=self.load_dataset)
+        processing_panel = HorizontalBoxLayout(size_hint=(None, None), size=(130, 40))
+        self.discretize_button = ToggleButton(text='Discretize')
+        self.scale_button = ToggleButton(text='Scale')
+        for btn_proc in [self.discretize_button, self.scale_button]:
+            btn_proc.state = 'normal'
+            btn_proc.group = 'preprocessing'
+            btn_proc.bind(state=self.select_preprocessing)
+            processing_panel.add_widget(btn_proc)
 
-        dataset_panel = HorizontalBoxLayout(size_hint=(None, None), size=(130, 40))
-        dataset_panel.add_widget(self.data_options)
-        dataset_panel.add_widget(self.load_button)
-
-        left_sidebar = VerticalBoxLayout(size_hint_x=None, width=330, padding=15, spacing=15)
-        left_sidebar.add_widget(Label())
+        left_sidebar = SidebarBoxLayout()
         left_sidebar.add_widget(task_panel)
-        left_sidebar.add_widget(dataset_panel)
+        left_sidebar.add_widget(self.main_panel)
+        left_sidebar.add_widget(processing_panel)
         left_sidebar.add_widget(Label())
 
         self.add_widget(left_sidebar)
-
-        dataset_info_panel = VerticalBoxLayout(spacing=15, size_hint_x=None, width=300)
-        dataset_info_panel.add_widget(Label())
-        dataset_info_panel.add_widget(Label(text='Dataset info'))
-        dataset_info_panel.add_widget(self.info_label)
-        dataset_info_panel.add_widget(Label())
-
-        self.add_widget(dataset_info_panel)
-
-        dataset_option_panel = VerticalBoxLayout(size_hint_x=None, width=500)
-        preprocessing_panel = HorizontalBoxLayout(spacing=25, size_hint_y=None, height=40)
-        preprocessing_panel.add_widget(Label(text='Dataset options'))
-        self.discretize_button = ToggleButton(text='Discretize', group='preprocessing', state='normal')
-        self.scale_button = ToggleButton(text='Scale', group='preprocessing')
-        for btn_proc in [self.discretize_button, self.scale_button]:
-            btn_proc.bind(state=self.select_preprocessing)
-            preprocessing_panel.add_widget(btn_proc)
-        dataset_option_panel.add_widget(Label())
-        dataset_option_panel.add_widget(preprocessing_panel)
-        dataset_option_panel.add_widget(Label())
-
-        self.add_widget(dataset_option_panel)
+        self.add_widget(self.info_label)
 
         self.init_datasets()
 
-    def select_dataset(self, spinner, text):
+    def select(self, spinner, text):
         self.dataset = text if text != DATASET_MESSAGE else None
-        self.load_button.disabled = False
+        self.go_button.disabled = False
 
-    def load_dataset(self, button):
-        self.controller.load_dataset(self.dataset)
-        self.discretize_button.disabled = False
-        self.scale_button.disabled = False
+    def go_action(self, button):
+        self.controller.load_dataset()
+        if self.task == 'Classification':
+            self.discretize_button.disabled = False
+        else:
+            self.scale_button.disabled = False
 
     def set_dataset_info(self):
-        data, classes = self.controller.data
+        data = self.controller.data
         self.info_label.text = INFO_DATASET_MESSAGE if data is None else \
+            INFO_DATASET_PREFIX + \
             f'Dataset: {self.dataset}\nInput variables: {len(data.columns) - 1}\nInstances: {len(data)}'
-        if classes is not None and isinstance(classes[0], str):
-            self.info_label.text += f'\nClasses: {len(np.unique(classes))}'
+        if data is not None and isinstance(data.iloc[0, -1], str):
+            self.info_label.text += f'\nClasses: {len(np.unique(data.iloc[:, -1]))}'
+        self.info_label.text += '\n'
 
     def init_datasets(self):
-        self.data_options.text = DATASET_MESSAGE
-        self.data_options.values = [entry[0] for entry in DATASETS if self.task in entry[1]]
-        self.load_button.disabled = True
+        self.spinner_options.text = DATASET_MESSAGE
+        self.spinner_options.values = [entry[0] for entry in DATASETS if self.task in entry[1]]
+        self.go_button.disabled = True
         self.discretize_button.state = 'normal'
         self.scale_button.state = 'normal'
         self.discretize_button.disabled = True
