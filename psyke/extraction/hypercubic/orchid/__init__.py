33a5b4c9e66a274895819667722d8e6e65e0ff6f
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2022-08-21 00:36:09 +0200

    chore(gui): ref

--- psyke/extraction/hypercubic/orchid/__init__.py
+++ psyke/extraction/hypercubic/orchid/__init__.py
@@ -13,6 +13,5 @@ class ORCHiD(CReEPy):
 
     def __init__(self, predictor, depth: int, error_threshold: float, output: Target = Target.CONSTANT,
                  gauss_components: int = 5, ranks: list[(str, float)] = [], ignore_threshold: float = 0.0):
-        super().__init__(predictor, depth, error_threshold, output, gauss_components, ranks, ignore_threshold)
-        self.clustering = Extractor.cream(depth, error_threshold, Target.CLASSIFICATION if
-                                          isinstance(predictor, ClassifierMixin) else output, gauss_components)
+        super().__init__(predictor, depth, error_threshold, output, gauss_components, ranks, ignore_threshold,
+                         clustering=Extractor.cream)
