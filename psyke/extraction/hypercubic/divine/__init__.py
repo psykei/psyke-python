d97bec366dca55d3bb94235f7570dbb92fcefdf0
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2023-06-05 02:56:36 +0200

    feat(divine): added option for vicinity to corners

--- psyke/extraction/hypercubic/divine/__init__.py
+++ psyke/extraction/hypercubic/divine/__init__.py
@@ -15,10 +15,12 @@ class DiViNE(HyperCubeExtractor):
     Explanator implementing DiViNE algorithm.
     """
 
-    def __init__(self, predictor, k: int = 5, patience: int = 15, discretization=None, normalization=None):
+    def __init__(self, predictor, k: int = 5, patience: int = 15, close_to_center: bool = True,
+                 discretization=None, normalization=None):
         super().__init__(predictor, Target.CLASSIFICATION, discretization, normalization)
         self.k = k
         self.patience = patience
+        self.vicinity_function = DiViNE.__closest_to_center if close_to_center else DiViNE.__closest_to_corners
 
     @staticmethod
     def __pop(data: pd.DataFrame, idx: int = None) -> (Point, pd.DataFrame):
@@ -45,6 +47,24 @@ class DiViNE(HyperCubeExtractor):
         # instances with neighbors of different classes are discarded
         return data[count == 1]
 
+    def __sort_cubes(self):
+        cubes = [(cube.diversity, i, cube) for i, cube in enumerate(self._hypercubes)]
+        cubes.sort()
+        self._hypercubes = [cube[2] for cube in cubes]
+
+    def __closest(self, data: pd.DataFrame, cube: GenericCube) -> (Point, pd.DataFrame):
+        tree = BallTree(data.iloc[:, :-1], leaf_size=2)
+        return DiViNE.__pop(data,self.vicinity_function(tree, cube))
+
+    @staticmethod
+    def __closest_to_center(tree: BallTree, cube: GenericCube):
+        return tree.query([list(cube.center().dimensions.values())], k=1)[1][0][-1]
+
+    @staticmethod
+    def __closest_to_corners(tree: BallTree, cube: GenericCube):
+        distance, idx = tree.query([list(point.dimensions.values()) for point in cube.corners()], k=1)
+        return idx[np.argmin(distance)][-1]
+
     def _extract(self, dataframe: pd.DataFrame, mapping: dict[str: int] = None, sort: bool = True) -> Theory:
         data = self.__clean(dataframe)
 
@@ -55,16 +75,17 @@ class DiViNE(HyperCubeExtractor):
             cube = self.__to_cube(point)
 
             while patience > 0 and len(data) > 0:
-                tree = BallTree(data.iloc[:, :-1], leaf_size=2)
-                _, idx = tree.query([list(cube.center().dimensions.values())], k=1)
-                other, data = DiViNE.__pop(data, idx[0][-1])
+                other, data = self.__closest(data, cube)
                 if cube.output == list(other.dimensions.values())[-1]:
                     cube = cube.merge_with_point(other)
                     data = data[~(cube.filter_indices(data.iloc[:, :-1]))].reset_index(drop=True)
                 else:
                     patience -= 1
                     discarded.append(other)
-            self._hypercubes.append(cube)
+            if cube.volume() > 0:
+                cube.update(dataframe, self.predictor)
+                self._hypercubes.append(cube)
             if len(discarded) > 0:
                 data = pd.concat([data] + [d.to_dataframe() for d in discarded]).reset_index(drop=True)
+        self.__sort_cubes()
         return self._create_theory(dataframe, sort)
