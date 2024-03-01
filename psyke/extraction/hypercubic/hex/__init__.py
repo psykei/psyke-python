1c27e2709dbc8892be74c253e1a17a363e6b5c7d
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2024-03-01 03:27:30 +0100

    chore(HEx): enabled ghost cubes

--- psyke/extraction/hypercubic/hex/__init__.py
+++ psyke/extraction/hypercubic/hex/__init__.py
@@ -29,23 +29,27 @@ class HEx(GridEx):
     def _gain(self, parent_cube: GenericCube, new_cube: GenericCube) -> float:
         if isinstance(parent_cube, ClassificationCube):
             return parent_cube.output != new_cube.output
-        return parent_cube.error - new_cube.error > self.threshold * .7
+        return parent_cube.error - new_cube.error > self.threshold * .6
 
     def _iterate(self, surrounding: HyperCube, dataframe: pd.DataFrame):
         fake = dataframe.copy()
         surrounding.update(dataframe, self.predictor)
-        prev = [surrounding]
+        prev = [(None, surrounding, True)]
 
         for iteration in self.grid.iterate():
             next_iteration = []
-            for cube in prev:
+            for (parent, cube, gain) in prev:
                 subcubes, fake = self._cubes_to_split(cube, surrounding, iteration, dataframe, fake, True)
-                cleaned = [c for c in subcubes if c.count(dataframe) > 0 and self._gain(cube, c)]
-                if len(subcubes) > len(cleaned):
-                    if len(cleaned) > 0:
-                        idx = np.any([c.filter_indices(fake.iloc[:, :-1]) for c in cleaned], axis=0)
-                        cube.update(fake[~idx], self.predictor)
-                    self._hypercubes = [cube] + self._hypercubes
-                next_iteration += self._merge(cleaned, fake)
+                parent_idx = cube.filter_indices(fake.iloc[:, :-1])
+                cube = cube if gain else parent
+                cleaned = [(c, self._gain(cube, c)) for c in subcubes if c.count(dataframe) > 0]
+                eligible_idx = np.any([c.filter_indices(fake.iloc[:, :-1]) for c, g in cleaned if g], axis=0)
+                if sum(g for _, g in cleaned) > 0 and sum(parent_idx) > sum(eligible_idx) and gain:
+                    cube.update(fake[parent_idx & ~eligible_idx], self.predictor)
+                    if parent and self._gain(parent, cube):
+                        self._hypercubes = [cube] + self._hypercubes
+                next_iteration += [(cube, c, self._gain(cube, c)) for c in self._merge([c for c, _ in cleaned], fake)]
             prev = next_iteration.copy()
-        self._hypercubes = [cube for cube in prev] + self._hypercubes
+        self._hypercubes = [cube for (_, cube, gain) in prev if gain] + self._hypercubes
+        if min(np.any([c.filter_indices(dataframe.iloc[:, :-1]) for c in self._hypercubes[:-1]], axis=0)):
+            self._hypercubes = self._hypercubes[:-1]
