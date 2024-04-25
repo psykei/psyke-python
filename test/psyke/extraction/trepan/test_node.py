3b99321c7407460f0bb0fe7979310ab62aae8c02
Federico Sabbatini <federico.sabbatini2@studio.unibo.it>
2024-04-25 16:38:15 +0200

    [wip] CART tests

--- test/psyke/extraction/trepan/test_node.py
+++ test/psyke/extraction/trepan/test_node.py
@@ -10,42 +10,42 @@ class TestNode(unittest.TestCase):
     n_examples = dataset.shape[0]
     all_node = Node(dataset, n_examples)
     setosa_40 = Node(dataset.iloc[10:70, :], n_examples)
-    versicolor_10 = Node(dataset.iloc[95:110, :], n_examples)
-    virginica_50 = Node(dataset.iloc[20:130, :], n_examples)
+    virginica_10 = Node(dataset.iloc[95:110, :], n_examples)
+    versicolor_50 = Node(dataset.iloc[20:130, :], n_examples)
 
     def test_reach(self):
         node = Node(self.dataset, self.n_examples)
         self.assertEqual(node.reach, self.all_node.reach)
-        self.assertTrue(self.versicolor_10.reach < self.setosa_40.reach)
-        self.assertTrue(self.setosa_40.reach < self.virginica_50.reach)
-        self.assertTrue(self.virginica_50.reach < self.all_node.reach)
+        self.assertTrue(self.virginica_10.reach < self.setosa_40.reach)
+        self.assertTrue(self.setosa_40.reach < self.versicolor_50.reach)
+        self.assertTrue(self.versicolor_50.reach < self.all_node.reach)
 
     def test_dominant(self):
         self.assertEqual('setosa', self.setosa_40.dominant)
-        self.assertEqual('virginica', self.virginica_50.dominant)
-        self.assertEqual('versicolor', self.versicolor_10.dominant)
+        self.assertEqual('virginica', self.virginica_10.dominant)
+        self.assertEqual('versicolor', self.versicolor_50.dominant)
 
     def test_correct(self):
-        self.assertEqual(50, self.virginica_50.correct)
+        self.assertEqual(50, self.versicolor_50.correct)
         self.assertEqual(40, self.setosa_40.correct)
-        self.assertEqual(10, self.versicolor_10.correct)
+        self.assertEqual(10, self.virginica_10.correct)
 
     def test_fidelity(self):
         self.assertEqual(50 / 150, self.all_node.fidelity)
         self.assertEqual(40 / 60, self.setosa_40.fidelity)
-        self.assertEqual(10 / 15, self.versicolor_10.fidelity)
-        self.assertEqual(50 / 110, self.virginica_50.fidelity)
+        self.assertEqual(10 / 15, self.virginica_10.fidelity)
+        self.assertEqual(50 / 110, self.versicolor_50.fidelity)
 
     def test_priority(self):
-        self.assertTrue(self.all_node.priority < self.virginica_50.priority)
-        self.assertTrue(self.virginica_50.priority < self.setosa_40.priority)
-        self.assertTrue(self.setosa_40.priority < self.versicolor_10.priority)
+        self.assertTrue(self.all_node.priority < self.versicolor_50.priority)
+        self.assertTrue(self.versicolor_50.priority < self.setosa_40.priority)
+        self.assertTrue(self.setosa_40.priority < self.virginica_10.priority)
 
     def test_n_classes(self):
         self.assertEqual(3, self.all_node.n_classes)
-        self.assertEqual(2, self.versicolor_10.n_classes)
+        self.assertEqual(2, self.virginica_10.n_classes)
         self.assertEqual(2, self.setosa_40.n_classes)
-        self.assertEqual(3, self.virginica_50.n_classes)
+        self.assertEqual(3, self.versicolor_50.n_classes)
         self.assertEqual(1, Node(self.dataset.iloc[15:40, :], self.n_examples).n_classes)
 
     def test_iterator(self):
@@ -65,8 +65,8 @@ class TestNode(unittest.TestCase):
         node = Node(self.dataset, self.n_examples, (('V1', 0.0), ('V2', 1.0)))
         self.assertEqual(' = setosa', str(self.all_node))
         self.assertEqual(' = setosa', str(self.setosa_40))
-        self.assertEqual(' = virginica', str(self.virginica_50))
-        self.assertEqual(' = versicolor', str(self.versicolor_10))
+        self.assertEqual(' = virginica', str(self.versicolor_50))
+        self.assertEqual(' = versicolor', str(self.virginica_10))
         self.assertEqual('!V1, V2 = setosa', str(node))
 
 
