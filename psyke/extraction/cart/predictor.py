4b113b88d6d316f6ea6c819faa4e5eb764e9cd5a
Andrea Terenziani <andrea.terenziani.at@gmail.com>
2024-06-06 23:38:20 +0200

    Fix incorrect import in predictor.py
    In Python 3.10, the `Iterable` abstract class has been removed from the `collections` module and has since been in `collections.abc` (see https://stackoverflow.com/a/72032154/4251579)

--- psyke/extraction/cart/predictor.py
+++ psyke/extraction/cart/predictor.py
@@ -1,4 +1,4 @@
-from collections import Iterable
+from collections.abc import Iterable
 from typing import Union, Any
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
